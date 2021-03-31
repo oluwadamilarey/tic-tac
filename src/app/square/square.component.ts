883126c742df7ea7edfed834fa0603710379c4f9
hardamilare <abimbola301@gmail.com>
2021-03-31 19:48:42 +0100

    square component created

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styles: [
+  ]
+})
+export class SquareComponent {
+
+}
